a98dbacd9a975c387cc09061c98c4a067ab9f09d
Maxon298 <83597635+Maxon298@users.noreply.github.com>
2025-02-28 10:58:59 +0500

    adding photos from gDrive

--- script.js
+++ script.js
@@ -18,16 +18,13 @@ for (let infoBtn of infoBtns) {
 }
 
 
+
+const API_URL = 'https://script.google.com/macros/s/AKfycbx1cPKac4KOVO_x86jQp8Me91wukv6E-3ITpA1AXBLtQrMa8iy21Q4Vb0iL5W6rm1LQow/exec'
 let instaContainer = document.querySelector('.instagram')
 
-let photos = [
-    '/assets/images/tattoos/dp.jpg',
-    '/assets/images/tattoos/dp.jpg',
-    '/assets/images/tattoos/dp.jpg',
-    '/assets/images/tattoos/WIN_20250228_10_21_43_Pro.jpg',
-    
-    
-]
+
+
+
 let mask = document.querySelector('.shadow-mask')
 mask.addEventListener('click',()=>{
     mask.style.display = 'none'
@@ -35,21 +32,27 @@ mask.addEventListener('click',()=>{
 
 let photoBig = document.querySelector('.instagram__photo__big')
 
-function drawPhotos(){
-
+function drawPhotos(photos){
+    console.table(photos)
     photos.forEach((photo, i)=>{
         instaContainer.innerHTML += `<div class="instagram__photo"></div>`
+        
     })
     
     let instaPhotos = document.querySelectorAll('.instagram__photo')
     instaPhotos.forEach((div, i)=>{
-        div.style.backgroundImage = `url(${photos[i]})`
-
+        div.style.backgroundImage = `url(${photos[i].url})`
+        
         div.addEventListener('click', ()=>{
             mask.style.display = 'block'
-            photoBig.style.backgroundImage = `url(${photos[i]})`
+            photoBig.style.backgroundImage = `url(${photos[i].url})`
         })
     })
 }
 
-drawPhotos()
+async function loadPhotos() {
+    let response = await fetch(API_URL);
+    let photos = await response.json();
+    drawPhotos(photos)
+}
+loadPhotos()
