45515ea8127d5dd9ef3172ad234f6f08e6ace5ca
Maxon298 <83597635+Maxon298@users.noreply.github.com>
2025-02-28 10:24:31 +0500

    tat photos can open now, added easily from js

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function openInfoTab(infoBtn){
     
     let plus = infoBtn.querySelector('.vert')
     plus.classList.toggle('vert_off')
+
+    infoBtn.scrollIntoView({behavior: 'smooth', block: 'start'})
+    
     
 }
 
@@ -14,3 +17,39 @@ for (let infoBtn of infoBtns) {
     infoBtn.addEventListener('click', ()=> openInfoTab(infoBtn))
 }
 
+
+let instaContainer = document.querySelector('.instagram')
+
+let photos = [
+    '/assets/images/tattoos/dp.jpg',
+    '/assets/images/tattoos/dp.jpg',
+    '/assets/images/tattoos/dp.jpg',
+    '/assets/images/tattoos/WIN_20250228_10_21_43_Pro.jpg',
+    
+    
+]
+let mask = document.querySelector('.shadow-mask')
+mask.addEventListener('click',()=>{
+    mask.style.display = 'none'
+})
+
+let photoBig = document.querySelector('.instagram__photo__big')
+
+function drawPhotos(){
+
+    photos.forEach((photo, i)=>{
+        instaContainer.innerHTML += `<div class="instagram__photo"></div>`
+    })
+    
+    let instaPhotos = document.querySelectorAll('.instagram__photo')
+    instaPhotos.forEach((div, i)=>{
+        div.style.backgroundImage = `url(${photos[i]})`
+
+        div.addEventListener('click', ()=>{
+            mask.style.display = 'block'
+            photoBig.style.backgroundImage = `url(${photos[i]})`
+        })
+    })
+}
+
+drawPhotos()
